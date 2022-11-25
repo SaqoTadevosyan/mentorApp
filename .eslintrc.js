42aa83896b756115ac73eff70d9b62263180b7e1
Sargis Tadevosyan <sargistadevosyan14@gmail.com>
2022-11-25 18:04:32 +0400

    feat:added config for eslint,prettier,added some libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,45 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+  extends: ['@react-native-community', 'plugin:import/typescript'],
+  plugins: ['jest', 'import'],
+  rules: {
+    quotes: [2, 'double', {avoidEscape: true}],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['off'],
+    'react/no-unstable-nested-components': ['off'],
+    "react-native/no-inline-styles": 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-native'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
       },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
     },
-  ],
+  },
 };
