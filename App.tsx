b8445557ea078562dffc1813cc8d1106685045b3
Sargis Tadevosyan <sargistadevosyan14@gmail.com>
2022-11-28 00:32:13 +0400

    fix:fixed android gesture issue

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Host } from "react-native-portalize";
 import { Provider } from "react-redux";
 import "react-native-gesture-handler";
@@ -9,11 +10,13 @@ import { store } from "./src/store";
 
 const App = () => {
   return (
-    <Host>
-      <Provider store={store}>
-        <BottomTabNavigator />
-      </Provider>
-    </Host>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Host>
+        <Provider store={store}>
+          <BottomTabNavigator />
+        </Provider>
+      </Host>
+    </GestureHandlerRootView>
   );
 };
 
